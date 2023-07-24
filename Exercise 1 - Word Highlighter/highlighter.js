7744a80c72b3cd9270c12942b0ff2d7d8c23d528
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-07-24 09:51:16 +0200

    Ojemba Assessment

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,15 @@
+const myPara = document.getElementById("#myParagraph");
+
+function highlighterWord(arr) {
+  let strArr = arr[0].split(" ");
+  for (let i in strArr) {
+    myPara = Array.from(matches).forEach((m, index) =>
+      m.replaceWith(
+        "<span style='background-color: yellow; text-decoration'>" +
+          `${index > 0 ? "" : ""}${m}</span>`
+      )
+    );
+  }
+}
+
 console.log("Exercise 1 - Word Highlighter");
--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,27 @@
-console.log("Exercise 3 - Timer");
+const timer = document.getElementById("myPar");
+let second = 30;
+let plusAny = document.getElementById("plus");
+plusAny.addEventListener("click", function () {
+  return (second = second + 1);
+});
+let minusAny = document.getElementById("minus");
+minusAny.addEventListener("click", function () {
+  return (second = second - 1);
+});
+let start = document.getElementById("start");
+start.addEventListener("click", function () {
+  return (second = 30);
+  start.innerHTML = "STOP";
+});
+let reset = document.getElementById("reset");
+reset.addEventListener("click", function () {
+  return (second = 0);
+});
+
+let stopWatch = setInterval(function () {
+  if (second <= 0) {
+    clearInterval(stopWatch);
+  }
+  timer.textContent = second;
+  second--;
+}, 1000);
